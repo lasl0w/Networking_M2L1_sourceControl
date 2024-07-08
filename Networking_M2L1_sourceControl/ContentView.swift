a137a7458f0a8dee6cb0eb9e5bf00800c9585c8b
Tom Montgomery <montgomery3681@gmail.com>
2024-07-08 14:27:35 -0600

    Initial Commit

---
+++ Networking_M2L1_sourceControl/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Networking_M2L1_sourceControl
+//
+//  Created by tom montgomery on 7/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Networking_M2L1_sourceControl/Networking_M2L1_sourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  Networking_M2L1_sourceControlApp.swift
+//  Networking_M2L1_sourceControl
+//
+//  Created by tom montgomery on 7/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Networking_M2L1_sourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
